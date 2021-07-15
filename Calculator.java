7f4b18ba7c6c393a09d1615d6ff07c31b9f517c8
unknown <ibmb1java15@iiht.tech>
2021-07-15 16:05:20 +0530

    Calculator.java with 3 method

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator
+{
+  public static int square(nt x)
+  {
+    return x*x;
+  }
+  public static int sum(int x, int y)
+  {
+    return x+y;
+  }
+  public static int power(int x, int y)
+  {
+    int res = 1;
+    for(;y>0;y--)
+      res =res *x;
+    return res;
+  }
+}
